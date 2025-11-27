607d88c6510d9f9d4928d745552fe19849d539b8
0147.19 <151461411+thanh0147@users.noreply.github.com>
2025-11-27 22:11:58 +0700

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,8 @@
 import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from database import Base # Dòng này chắc chắn đã có sẵn ở đầu file của bạn
 
 load_dotenv()
 
@@ -12,3 +14,10 @@ def get_supabase() -> Client:
         raise ValueError("Missing SUPABASE_URL or SUPABASE_KEY in .env")
 
     return create_client(SUPABASE_URL, SUPABASE_KEY)
+
+class WallMessage(Base):
+    __tablename__ = "wall_messages"
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+    emotion_color = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
