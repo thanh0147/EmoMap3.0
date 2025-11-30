4e5efa0fc6bed284d7aca24c4b46610e7f2915ee
0147.19 <txt0147.03@gmail.com>
2025-11-30 22:14:29 +0700

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,3 +8,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+function toggleChat() {
+  var chatWindow = document.getElementById("myChatWindow");
+  var btn = document.querySelector(".chat-btn-float");
+
+  if (chatWindow.style.display === "none" || chatWindow.style.display === "") {
+      chatWindow.style.display = "flex"; // Hiện chat box
+      btn.innerHTML = "✖"; // Đổi icon nút thành dấu X
+  } else {
+      chatWindow.style.display = "none"; // Ẩn chat box
+      btn.innerHTML = "💬"; // Đổi icon nút về lại chat
+  }
+}
